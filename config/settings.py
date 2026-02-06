2f339331d0b964505489fe242c501a38f7827efc
Zane Moon <zanem201@gmail.com>
2026-02-05 21:52:25 -0800

    Set up S3 media Storage

--- config/settings.py
+++ config/settings.py
@@ -48,6 +48,7 @@ INSTALLED_APPS = [
     'picks',
     'schedule',
     'users',
+    'storages',
 ]
 
 MIDDLEWARE = [
@@ -134,6 +135,16 @@ STATICFILES_STORAGE  = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 MEDIA_URL = 'media/'
 MEDIA_ROOT = BASE_DIR / 'media'
+
+# Use S3 for media files in production
+if os.environ.get('USE_S3') == 'True':
+    DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+
+    AWS_STORAGE_BUCKET_NAME = os.environ['AWS_STORAGE_BUCKET_NAME']
+    AWS_S3_REGION_NAME = os.environ.get('AWS_S3_REGION_NAME', 'us-east-2')
+    AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
+
+    MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/'
 
 # Email Configuration for Password Reset
 # For development/testing - prints emails to console
